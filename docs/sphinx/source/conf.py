32dc770822962ac41389f7cbfc5822b4890f7ab0
mahendrapaipuri <mahendra.paipuri@gmail.com>
2023-11-02 16:35:17 +0100

    docs: Install package in docs env
    * We need package to be installed to get API docs

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -50,7 +50,7 @@ napoleon_use_ivar = True
 # autodoc_member_order = 'bysource'
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # Suppress warnings coming from duplicate labels
 # suppress_warnings = ['autosectionlabel.*']
@@ -126,7 +126,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
